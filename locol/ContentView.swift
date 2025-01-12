0272a0401eabd54bc6945f5432639dfed7beb4c2
Austin Parker <austin@ap2.io>
2025-01-12 11:07:25 -0500

    Initial Commit

---
+++ locol/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  locol
+//
+//  Created by Austin Parker on 1/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ locol/locolApp.swift
@@ -0,0 +1,17 @@
+//
+//  locolApp.swift
+//  locol
+//
+//  Created by Austin Parker on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct locolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
